e9598c035abfe391b1f6e7ead93119541ddb9932
eljimo <jimmy.mx.yuan@gmail.com>
2024-08-07 23:08:46 +1000

    Fix capitalism

--- src/pages/resume.js
+++ src/pages/resume.js
@@ -3,7 +3,7 @@ import Row from 'react-bootstrap/Container';
 import Col from 'react-bootstrap/Container';
 import Button from 'react-bootstrap/Button';
 import { AiOutlineDownload } from "react-icons/ai";
-import Image from 'react-bootstrap/image'
+import Image from 'react-bootstrap/Image'
 import Layout from '@/components/layout';
 
 
